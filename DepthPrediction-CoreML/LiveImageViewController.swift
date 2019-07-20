c0f9ba9b41f459987ccff051f678ecbeb3bb5c71
tucan9389 <tucan9389@gmail.com>
2019-07-20 15:21:10 +0900

    Add LiveImageViewController

---
+++ DepthPrediction-CoreML/LiveImageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LiveImageViewController.swift
+//  DepthPrediction-CoreML
+//
+//  Created by Doyoung Gwak on 20/07/2019.
+//  Copyright © 2019 Doyoung Gwak. All rights reserved.
+//
+
+import UIKit
+
+class LiveImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
