705b9d7dd31a71f67343d1f790e8e1afb39367cb
tucan9389 <tucan9389@gmail.com>
2019-07-20 13:48:13 +0900

    Upload new project

---
+++ DepthPrediction-CoreML/StillImageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DepthPrediction-CoreML
+//
+//  Created by Doyoung Gwak on 20/07/2019.
+//  Copyright © 2019 Doyoung Gwak. All rights reserved.
+//
+
+import UIKit
+
+class StillImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
